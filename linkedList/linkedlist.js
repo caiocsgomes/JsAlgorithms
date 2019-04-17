7e06a668227f11c070d910a9e291fed7772306a2
Caio Gomes <caiocsgomes@gmail.com>
2019-04-16 21:01:38 -0300

    Improving linked list

--- linkedList/linkedlist.js
+++ linkedList/linkedlist.js
@@ -1,13 +1,45 @@
-let Node = function(data, next = null){
+let Node = function(data){
     this.data = data;
-    this.next = next;
+    this.next = null;
+    this.previous = null;
 }
 let Linkedlist = function(){
     this.head = null;
+    this.tail = null;
 } 
 linkedlist.prototype.insertFirst = function(data){
-    let node = new Node(data, this.head);
-    this.head = node;
+    let newNode = new Node(data);
+    if(this.head){
+        this.head.previous = newNode;
+        newNode.next = this.head;
+    } else {
+        this.tail = newNode;
+    }
+    this.head = newNode;
+    
+}
+linkedlist.prototype.getIndex = function(index){
+    if(index < 0 || (this.size() - 1) < index) return null;
+    counter = 0;
+    current = this.head;
+    while(counter < index){
+        current = current.next;
+        counter++;
+    }
+    return current.data;
+}
+linkedlist.prototype.removeIndex = function(index){
+    //TODO: add function and test
+}
+linkedlist.prototype.insert = function(data){
+    let newNode = new Node(data);
+    if(this.tail){
+        this.tail.next = newNode;
+        newNode.previous = this.tail;
+    } else {
+        this.first = newNode;
+    }
+    this.tail = newNode;
 }
 linkedlist.prototype.size = function(){
     let current = this.head;
@@ -22,20 +54,16 @@ linkedlist.prototype.getFirst = function(){
     return this.head.data;
 }
 linkedlist.prototype.getLast = function(){
-    current = this.head;
-    if(!current) return null;
-    while(current.next) current = current.next;
-    return current.data;
+    return this.tail;
 }
 linkedlist.prototype.clear = function(){
-    this.head = null;
+    this.head = this.tail = null;
 }
 linkedlist.prototype.removeFirst = function(){
     this.head = this.head.next;
+    this.head.previous = null;
 }
 linkedlist.prototype.removeLast = function(){
-    current = this.head;
-    if(!current) return;
-    while(current.next.next) current = current.next;
-    current.next = null;
+    this.tail = this.tail.previous;
+    this.tail.next = null;
 }
